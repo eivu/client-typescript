68175b7386a47edc7195af5fe59b43283a4a020c
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-09-29 15:59:53 -0400

    fetch is working - dist is now part of gitignore

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.EIVU_SERVER_HOST + '/api/upload/v1/',
+  baseURL: process.env.EIVU_UPLOAD_SERVER_HOST + '/api/upload/v1/buckets/' + process.env.EIVU_BUCKET_UUID + '/',
   headers: {
     Authorization: 'Token ' + process.env.EIVU_USER_TOKEN,
     'Content-Type': 'application/json',
