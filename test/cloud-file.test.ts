41eb6857f2acbb929acabfc1e79141e6541c38ff
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-10-01 13:51:51 -0400

    first real cloud file test is passing

--- test/cloud-file.test.ts
+++ test/cloud-file.test.ts
@@ -6,26 +6,19 @@ import {AI_OVERLORDS_RESERVERATION} from './fixtures/responses'
 
 const SERVER_HOST = process.env.EIVU_UPLOAD_SERVER_HOST as string
 const BUCKET_UUID = process.env.EIVU_BUCKET_UUID
-// const USER_UUID = process.env.EIVU_USER_UUID
 const URL_BUCKET_PREFIX = `/api/upload/v1/buckets/${BUCKET_UUID}`
-
-// url: "#{Eivu::Client.configuration.host}/api/upload/v1/buckets/#{bucket_uuid}/cloud_files/#{md5}",
-//             method: :get,
-//             headers: { 'Authorization' => "Token #{Eivu::Client.configuration.user_token}" },
-//             verify_ssl: !Eivu::Client.configuration.ignore_ssl_cert
-//           )
-
-//           /api/upload/v1/buckets/#{bucket_uuid}/cloud_files/#{md5}"
 
 describe('CloudFile', () => {
   beforeEach(() => {
     nock.cleanAll()
-    // nock('http://localhost:5000').post(`/path`, data).log(console.log).reply(200, {id: 1})
   })
 
   it('fetches a cloud file by MD5 hash', async () => {
     const md5 = '7ED971313D1AEA1B6E2BF8AF24BED64A'
-    const req = nock(SERVER_HOST).get(`${URL_BUCKET_PREFIX}/cloud_files/${md5}`).reply(200, AI_OVERLORDS_RESERVERATION)
+    const req = nock(SERVER_HOST)
+      .get(`${URL_BUCKET_PREFIX}/cloud_files/${md5}`)
+      .query({keyFormat: 'camel_lower'})
+      .reply(200, AI_OVERLORDS_RESERVERATION)
 
     const cloudFile = await CloudFile.fetch(md5)
     expect(cloudFile).toBeDefined()
