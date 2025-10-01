93f81b1f86e70e40a9699a5ae2186a9a19dd64b1
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-10-01 12:30:55 -0400

    working on tests

--- test/cloud-file.test.ts
+++ test/cloud-file.test.ts
@@ -1,16 +1,35 @@
 import {describe, expect, it} from '@jest/globals'
+import nock from 'nock'
 
-// import {CloudFile} from '@src/cloud-file'
+import {CloudFile} from '../src/cloud-file'
 import {AI_OVERLORDS_RESERVERATION} from './fixtures/responses'
 
+const SERVER_HOST = process.env.EIVU_UPLOAD_SERVER_HOST as string
+const BUCKET_UUID = process.env.EIVU_BUCKET_UUID
+// const USER_UUID = process.env.EIVU_USER_UUID
+const URL_BUCKET_PREFIX = `/api/upload/v1/buckets/${BUCKET_UUID}`
+
+// url: "#{Eivu::Client.configuration.host}/api/upload/v1/buckets/#{bucket_uuid}/cloud_files/#{md5}",
+//             method: :get,
+//             headers: { 'Authorization' => "Token #{Eivu::Client.configuration.user_token}" },
+//             verify_ssl: !Eivu::Client.configuration.ignore_ssl_cert
+//           )
+
+//           /api/upload/v1/buckets/#{bucket_uuid}/cloud_files/#{md5}"
+
 describe('CloudFile', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+    // nock('http://localhost:5000').post(`/path`, data).log(console.log).reply(200, {id: 1})
+  })
+
   it('fetches a cloud file by MD5 hash', async () => {
-    console.log(`EIVU_BUCKET_UUID - ${process.env.EIVU_BUCKET_UUID}`)
-    // const md5 = 'F04CD103EDDFB64EFD8D9FC48F3023FD' // Example MD5 hash
-    // const cloudFile = await CloudFile.fetch(md5)
-    // expect(cloudFile).toBeInstanceOf(CloudFile)
-    // expect(cloudFile.attr.md5).toBe(md5)
-    expect(AI_OVERLORDS_RESERVERATION.md5).toBe('7ED971313D1AEA1B6E2BF8AF24BED64A')
-    expect(true).toBe(true) // Placeholder assertion
+    const md5 = '7ED971313D1AEA1B6E2BF8AF24BED64A'
+    const req = nock(SERVER_HOST).get(`${URL_BUCKET_PREFIX}/cloud_files/${md5}`).reply(200, AI_OVERLORDS_RESERVERATION)
+
+    const cloudFile = await CloudFile.fetch(md5)
+    expect(cloudFile).toBeDefined()
+    expect(cloudFile.attr).toEqual(AI_OVERLORDS_RESERVERATION)
+    expect(req.isDone()).toBe(true)
   })
 })
