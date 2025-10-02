aa115f7584a29021cd3bee71d28d934896fefacc
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-10-02 12:28:53 -0400

    tests for S3Uplaoder#md5AsFolders are passing

--- test/s3-uploader.test.ts
+++ test/s3-uploader.test.ts
@@ -40,4 +40,34 @@ describe('S3Uploader', () => {
       })
     })
   })
+
+  describe('md5AsFolders', () => {
+    describe('image: ai overlords', () => {
+      it('converts an MD5 hash into a folder structure', () => {
+        const remoteAttr = {...AI_OVERLORDS_RESERVATION}
+        const resourceType = 'image'
+        const asset = cleansedAssetName('test/fixtures/samples/image/ai overlords.jpg')
+
+        const cloudFile = new CloudFile({remoteAttr, resourceType})
+        const s3Uploader = new S3Uploader({asset, cloudFile})
+        expect(s3Uploader.md5AsFolders('7ED971313D1AEA1B6E2BF8AF24BED64A')).toBe(
+          '7E/D9/71/31/3D/1A/EA/1B/6E/2B/F8/AF/24/BE/D6/4A',
+        )
+      })
+    })
+
+    describe('video: big buck bunny', () => {
+      it('converts an MD5 hash into a folder structure', () => {
+        const remoteAttr = {...MOV_BBB_RESERVATION}
+        const resourceType = 'video'
+        const asset = cleansedAssetName('test/fixtures/samples/video/mov_bbb.mp4')
+
+        const cloudFile = new CloudFile({remoteAttr, resourceType})
+        const s3Uploader = new S3Uploader({asset, cloudFile})
+        expect(s3Uploader.md5AsFolders('198918F40ECC7CAB0FC4231ADAF67C96')).toBe(
+          '19/89/18/F4/0E/CC/7C/AB/0F/C4/23/1A/DA/F6/7C/96',
+        )
+      })
+    })
+  })
 })
