8db984825414558a861a7fc4e01d3de7fb7164bd
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-10-03 19:22:13 -0400

    added Artist and Release Type

---
+++ src/types/release.ts
@@ -0,0 +1,8 @@
+export type Release = {
+  artwork_md5?: null | string
+  bundle_pos?: null | string
+  name?: null | string
+  position?: null | string
+  primary_artist_name?: null | string
+  year?: null | string
+}
