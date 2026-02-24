c9e77a406ef19bec8d9e540d37116dc2d9c97adf
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2026-02-24 13:57:54 -0500

    disabled fs.stats warning

--- bin/dev.cmd
+++ bin/dev.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-node --loader ts-node/esm --no-warnings=ExperimentalWarning "%~dp0\dev" %*
+node --loader ts-node/esm --no-warnings=ExperimentalWarning --disable-warning=DEP0180 "%~dp0\dev" %*
