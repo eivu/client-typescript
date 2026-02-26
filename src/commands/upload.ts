87e60e07ffddce3a26328aac4775e98023f8daf4
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2026-02-25 21:40:19 -0500

    remote uploads are working with cli

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,4 +1,7 @@
 import {Args, Command, Flags} from '@oclif/core'
+import {Client} from '@src/client'
+import {isOnline} from '@src/utils'
+import * as fs from 'node:fs'
 
 export default class Upload extends Command {
   static override args = {
@@ -15,14 +18,39 @@ export default class Upload extends Command {
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Upload)
     const {nsfw, secured, filename} = flags
-    nsfw = nsfw ?? false
-    secured = secured ?? false
-    filename = filename ?? undefined
+    const {path} = args
+    const securedValue = secured ?? false
+    const nsfwValue = secured ?? nsfw ?? false
+    const filenameValue = filename ?? undefined
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/jinx/projects/eivu/client-typescript/src/commands/upload.ts`)
-    if (args.path && flags.force) {
-      this.log(`you input --force and --path: ${args.path}`)
+    if (!path) {
+      this.log('Please provide a path to a file, folder, or URL to upload.')
+      return
     }
+
+    const defaultArguments = {
+      nsfw: nsfwValue,
+      secured: securedValue,
+    }
+
+    const onlineCheck = await isOnline(path)
+    if (onlineCheck.isOnline) {
+      Client.uploadRemoteFile({
+        assetFilename: filenameValue,
+        downloadUrl: path,
+        ...defaultArguments,
+      })
+    } else if (fs.existsSync(path)) {
+      const stats = fs.statSync(path)
+      if (stats.isFile()) {
+        this.log(`Path is a file: ${path}`)
+      } else if (stats.isDirectory()) {
+        this.log(`Path is a folder: ${path}`)
+      }
+    } else {
+      this.log(`A valid resource could not be found: ${path}`)
+    }
+
+    // this.log('Upload command executed successfully.')
   }
 }
