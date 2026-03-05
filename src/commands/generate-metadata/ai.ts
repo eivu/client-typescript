6d4c286427e0fc8b0dfb3c4ee2f285d9c56a4de6
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2026-03-04 19:41:41 -0500

    wip

--- src/commands/generate-metadata/ai.ts
+++ src/commands/generate-metadata/ai.ts
@@ -1,12 +1,14 @@
 import {Args, Command, Flags} from '@oclif/core'
+import {MetadataGenerator} from '@src/ai/metadata-generator'
+import * as fs from 'node:fs'
 
 export default class GenerateMetadataAi extends Command {
   static override aliases = ['gm:ai']
   static override args = {
-    file: Args.string({description: 'file to read'}),
+    path: Args.string({description: 'path to file or folder or url to generate metadata for'}),
   }
-  static override description = 'describe the command here'
-  static override examples = ['<%= config.bin %> <%= command.id %>']
+  // static override description = 'describe the command here'
+  // static override examples = ['<%= config.bin %> <%= command.id %>']
   static override flags = {
     // flag with no value (-f, --force)
     force: Flags.boolean({char: 'f'}),
@@ -15,12 +17,36 @@ export default class GenerateMetadataAi extends Command {
   }
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(GenerateMetadataAi)
+    // const {args, flags} = await this.parse(GenerateMetadataAi)
+    const {args} = await this.parse(GenerateMetadataAi)
+    const {path} = args
+    let pathsArray: string[]
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/jinx/projects/eivu/client-typescript/src/commands/generate-metadata/ai.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    // const name = flags.name ?? 'world'
+    // this.log(`hello ${name} from /Users/jinx/projects/eivu/client-typescript/src/commands/generate-metadata/ai.ts`)
+    // if (args.file && flags.force) {
+    //   this.log(`you input --force and --file: ${args.file}`)
+    // }
+
+    if (!path) {
+      this.log('Please provide a path to a file or folder to generate metadata for.')
+      return
     }
+
+    if (fs.existsSync(path)) {
+      const stats = fs.statSync(path)
+      if (stats.isFile()) {
+        pathsArray = [path]
+      } else if (stats.isDirectory()) {
+        this.log('folders not supported yet, please provide a path to a file')
+        return
+      }
+    }
+
+    const results = await MetadataGenerator.generate(pathsArray, {
+      agent: 'claude',
+      apiKey: process.env.ANTHROPIC_API_KEY,
+      overwrite: false,
+    })
   }
 }
